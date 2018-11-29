59c604f9cfebf7df0d46df6cae61f5a321a99e8d
asoroosh <srafyouni@gmail.com>
2018-11-29 18:28:11 +0000

    housekeeping

--- Sims/MakeModularNetwork.m
+++ Sims/MakeModularNetwork.m
@@ -1,16 +1,38 @@
-function M = MakeModularNetwork(N,nM,modens)
+function M = MakeModularNetwork(N,nM,modens,NetMode)
+% simulate modular networks
+% SA, Ox, 2018
+%
+if ~exist('modens','var');  modens = 0.9; end; 
+if ~exist('NetMode','var'); NetMode = 'Circular'; end; 
 
-if ~exist('modens','var'); modens = 0.9; end; 
-
-%N = 100; 
-%nM = 5;
 sM = N./nM;
 ModulProb = ones(1,nM).*modens;
 
 for i = 1:nM
-
     Mtmp = binornd(1,ModulProb(i),[sM sM]);
     %density_und(Mtmp)
     M((i-1)*sM+1:i*sM,(i-1)*sM+1:i*sM) = triu(Mtmp,1) + triu(Mtmp,1)';
+    
+    connectornode(i) = randi([(i-1)*sM+1 i*sM]);
+end
 
+
+%If you want to have the connectors be fully connected to each other
+
+switch NetMode
+    case 'RichClub'
+        for i = connectornode'
+            for j = connectornode'
+               M(i,j) = 1; 
+            end
+        end
+    case 'Circular'
+        %If you wanna have them form a circle
+        for i = 1:numel(connectornode)-1
+            ii = connectornode(i); 
+            jj = connectornode(i+1);
+            M(ii,jj) = 1;
+        end
+    otherwise
+            error('choose either Circular or RichClub')
 end
--- Sims/Sim_Smoothness.m
+++ Sims/Sim_Smoothness.m
@@ -1,23 +1,61 @@
 
+clear
 
 N = 100; 
 nM = 4;
 sM = N./nM;
-M = MakeModularNetwork(N,nM,0.2);
+nRlz = 50; 
+Noise=0; RndSign=1;
 
-Xclean = []; 
-for i = 1:nM
-    RndSign = ((rand>0.5)*2-1);
-    Xclean = [Xclean;ones(sM,1).*i.*RndSign];
+CutOff = 5;
+
+%Remove everything BUT the last 20 components:
+lpH = eye(N); 
+lpH(1:N-CutOff,1:N-CutOff) = 0;
+
+%just keep the first 20 components:
+hpH = eye(N); 
+hpH(CutOff:end,CutOff:end) = 0;
+
+for itr = 1:1
+    M = MakeModularNetwork(N,nM,0.1,'RichClub');
+    M = M.*abs(rand(N)); %put positive weights 
+    
+    Xclean = []; 
+    for i = 1:nM-1
+        %RndSign = ((rand>0.5)*2-1);
+        Xclean = [Xclean;ones(sM,1).*i.*RndSign];
+    end
+
+    LastBlock = randi(5,[sM,1]).*((rand(sM,1)>0.5)*2-1); %high frequency
+    %LastBlock = ones(sM,1).*(i+1).*RndSign; %low frequency
+    Xclean = [Xclean;LastBlock];
+    
+%     %the last block
+%     RndSign = ((rand(sM)>0.5)*2-1);
+%     Xclean = [Xclean;3.*RndSign];
+    
+    
+    
+    %Noise = randn(1,N);
+    X = Xclean + Noise';
+    
+    myGSP_plot3D(M,X)
+    
+    %== Unfiltered
+    L = myGSP_LapMat(M);
+    Xh(itr) = myGSP_Smoothness(L,X);
+    
+    %== LP filtered
+    lpXf = myGSP_VertexFilter(L,X,lpH,'piecewise');
+    lpXf_tmp = myGSP_Smoothness(L,lpXf);
+    lpXh(itr) = lpXf_tmp;
+    %== HP filtered
+    hpXf = myGSP_VertexFilter(L,X,hpH,'piecewise');
+    hpXf_tmp = myGSP_Smoothness(L,hpXf);
+    hpXh(itr) = hpXf_tmp;
+    
+    clear *_tmp
 end
 
-%Noise = rand(1,N);
-Noise = 0;
-
-X = Xclean + Noise';
-%X = Xclean; 
-
-L = myGSP_LapMat(M,'normalise');
-Xh = myGSP_Smoothness(L,X)
-
-figure; imagesc(M)
+disp(['all: ' num2str(mean(Xh)) ', l:' num2str(mean(lpXh)) ', h:' num2str(mean(hpXh))])
