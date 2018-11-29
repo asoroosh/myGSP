7cc7e0b0267a89ea300fcef96d3338553741cfa1
asoroosh <srafyouni@gmail.com>
2018-11-29 18:28:36 +0000

    HouseKeeping

--- myGSP_LapMat.m
+++ myGSP_LapMat.m
@@ -16,10 +16,23 @@ A(1:N+1:end) = 0;
 D = sum(A,2);
 
 if sum(strcmpi(varargin,'normalise'))
-    D(D~=0) = sqrt(1./D(D~=0));
-    D = spdiags(D,0,speye(size(A,1)));
-    A = D*A*D;
-    L = speye(size(A,1))-A; % L = I-D^-1/2*W*D^-1/2
+    
+% What the fuck is this?! I can't remember how I arrived to this    
+%     D(D~=0) = sqrt(1./D(D~=0));
+%     D = spdiags(D,0,speye(size(A,1)));
+%     A0 = D*A*D;
+%     L0 = speye(size(A0,1))-A0; % L = I-D^-1/2 * W * D^-1/2
+
+    idx = D>0;
+    L = - A;
+    % Ln = D^(-1/2) L D^(-1/2)
+    L(idx,:) = bsxfun(@times, L(idx,:), 1./sqrt(D(idx)));
+    L(:,idx) = bsxfun(@times, L(:,idx), 1./sqrt(D(idx))');
+    % put back diagonal to identity
+    % Note: for disconnected nodes we should still have 1 on diagonal
+    % (limit of L for W -> 0)
+    L(1:N+1:end) = 1;
+
 else
     L  = diag(D)-A; 
 end
