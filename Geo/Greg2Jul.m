c6e6ab0697e87a6fc534902caed6baee49ff1f6d
asoroosh <srafyouni@gmail.com>
2019-01-26 16:33:23 +0000

    Geo folder was moved to UKBUtils

--- Geo/Greg2Jul.m
+++
@@ -1,7 +0,0 @@
-function jDate = Greg2Jul(Date)
-% for slash seperated dates, used in UKB
-% SA, Ox, 2018
-%
-DateNum = str2double(split(Date,'/'));
-t = datetime(DateNum(3),DateNum(2),DateNum(1));
-jDate = juliandate(t);
