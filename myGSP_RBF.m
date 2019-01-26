678929d5ad3e911554c49894467cf3008b55bd8d
asoroosh <srafyouni@gmail.com>
2019-01-26 16:36:11 +0000

    cleaned and moved to Geo

--- myGSP_RBF.m
+++
@@ -1,9 +0,0 @@
-function Topol = myGSP_RBF(DistMat,Sigma)
-
-if ~exist('Sigma','var')
-    idx   = find(triu(ones(size(DistMat,1)),1));
-    Sigma = mean(DistMat(idx));
-end
-
-Gamma = 1./(2*(Sigma.^2));
-Topol = exp(-(DistMat.^2)*Gamma);
